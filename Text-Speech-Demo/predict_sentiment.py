b5b0fdc62a1d433f47fa8923e378dfd201e20c32
Keshara Jayasinghe <kesharalahiru1@gmail.com>
2019-07-30 20:10:00 +0530

    Update predict_sentiment.py

--- Text-Speech-Demo/predict_sentiment.py
+++ Text-Speech-Demo/predict_sentiment.py
@@ -1,4 +1,6 @@
-# Edited Python script used in the predict sentiment experiment. (Sentiment Analysis Demo)
+# Edited Python script used in the predict sentiment experiment. 
+# To work with the web service (Sentiment Analysis Demo)
+# Source: Microsoft - Introduction to Artificial Intelligence (AI) course
 
 import pandas as pd
 
@@ -10,3 +12,13 @@ def azureml_main(dataset, stop_words);
     from nltk.stem.porter import PorterStemmer
     
     # Give the columns names and make a list of the tweets
+    dataset.columns = ['tweets']
+    tweets = dataset['tweets'].tolist()
+    
+    # For each tweet, remove punctuation and set to lower case
+    sp = string.punctuation
+    tweets = list(map(lambda t: ''.join(["" if c.isdigit() else c for c in t]), tweets))
+    tweets = list(map(lambda t: ''.join(["" if c in sp else c for c in t]), tweets))
+    tweets = list(map(str.lower, tweets))
+    
+    # Stem the tweet text
