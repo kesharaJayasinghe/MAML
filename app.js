e9c10b013a4b14a849b55400ab14d13c4de9a290
Keshara Jayasinghe <kesharalahiru1@gmail.com>
2019-08-06 00:35:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ var server = restify.createServer();
 serverlisten(process.env.port || process.env.PORT || 3978, function(){
   console.log('%s listening to %s', server.name, server.url);
 });
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+  appId: process.env.MicrosoftAppId,
+  appPassword: process.env.MicrosoftAppPassword,
+  openIdMetadata: process.env.BotOpenIdMetadata
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
